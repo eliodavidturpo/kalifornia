e87ccd241827a313096faf139aec678e34165b06
eliodavidturpo <eliodavidturpo@gmail.com>
2026-09-06 22:48:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// ===============================
+// CARRUSEL DE IMÁGENES DE PORTADA
+// ===============================
+
+const imagenesPortada = [
+    "images/zapatilla roja.jpg",
+    "images/zapatilla azul.jpg",
+    "images/zapatilla negra.jpg"
+];
+
+let imagenActual = 0;
+
+function cambiarImagenPortada() {
+
+    imagenActual++;
+
+    if (imagenActual >= imagenesPortada.length) {
+        imagenActual = 0;
+    }
+
+    const hero = document.querySelector(".hero");
+
+    hero.style.backgroundImage =
+        `linear-gradient(
+            90deg,
+            rgba(0,0,0,0.8),
+            rgba(0,0,0,0.2)
+        ),
+        url("${imagenesPortada[imagenActual]}")`;
+}
+
+// Cambiar cada 10 segundos
+setInterval(cambiarImagenPortada, 10000);
+
+
+// ===============================
+// CARRITO
+// ===============================
+
 let carrito = 0;
 
 function agregarCarrito() {
